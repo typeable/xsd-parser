d5be8ebe9d4d27ece97c4588d8a095298a47a7ed
Denis Redozubov <denis.redozubov@gmail.com>
2019-10-02 15:51:14 +0300

    Initial datatype spec

--- src/Text/XSD/Internal.hs
+++ src/Text/XSD/Internal.hs
@@ -5,13 +5,110 @@ module Text.XSD.Internal where
 
 import Control.Lens
 import Data.Text
+import Data.Map as M
+
+
+-- | Roughly based on https://www.w3.org/TR/xmlschema11-2/#built-in-datatypes
+data Datatype
+  = ComplexType ComplexType
+  | SimpleType SimpleType
+  deriving (Show)
+
+-- | Based on https://www.w3.org/TR/xmlschema11-1/#dcl.ctd.ctsc
+-- Only sequence and choice are supported
+data ComplexType
+  = CTSequence
+  | CTChoice
+  deriving (Show)
+
+-- | ENTITIES, IDREFS and NMTOKENS are not supported right now.
+data SimpleType
+  = STAnyURI
+  | STBase64Binary
+  | STBoolean
+  | STDate
+  | STDateTime
+  | STDecimal DecimalType
+  | STDouble
+  | STDuration DurationType
+  | STFloat
+  | STGDay
+  | STGMonth
+  | STGMonthDay
+  | STGYear
+  | STGYearMonth
+  | STHexBinary
+  | STNOTATION
+  | STQName QName
+  | STString StringType
+  | STTime
+  | STUnion -- special - xs:union
+  | STList  -- special - xs:list
+  deriving (Show)
+
+data StringType
+  = StrNormalizedString
+  | StrToken
+  | StrLanguage
+  | StrName
+  | StrNCNAME
+  | StrENTITY
+  | StrID
+  | StrIDREF
+  | StrNMTOKEN
+  deriving (Show)
+
+data DurationType = DayTimeDuration | YearMonthDuration deriving (Show, Eq)
+
+data DecimalType
+  = DTInteger
+  | DTLong
+  | DTInt
+  | DTShort
+  | DTByte
+  | DTNonNegativeInteger
+  | DTPositiveInteger
+  | DTUnsignedLong
+  | DTUnsignedInt
+  | DTUnsignedShort
+  | DTUnsignedByte
+  | DTNonPositiveInteger
+  | DTNegativeInteger
+  deriving (Show, Eq)
+
+type LocalName = Text
+
+type Namespace = Text
+
+type QName = (Namespace, LocalName)
+
+-- data Definition
+--   = XSString
+--   | XSSeq [Definition]
+--   | XSChoice [Definition]
+--   deriving (Show)
 
 type Attribute = (Text, Text)
 
-data Element = Element
-  { _attrs    :: [Attribute]
-  , _elements :: [Element] }
-  | Choice Element Element
+-- | Based on https://www.w3.org/TR/xmlschema11-1/#concepts-data-model
+-- Secondary and helper schema components are not supported right now.
+data SchemaComponent
+  = SCSimpleType SimpleType
+  | SCComplexType ComplexType
+  | SCElement Element
+  | SCAttribute
   deriving (Show)
 
-makeLenses ''Element
+data Element = Element
+  { name       :: String
+  , target     :: Maybe Namespace
+  , xtype      :: Datatype
+  , attrs      :: [Attribute]
+  , abstract   :: Bool
+  , elements :: [Element]
+  }
+  deriving (Show)
+
+-- | Embodies an XSD document
+newtype XSD = XSD { unXSD :: Element }
+  deriving (Show)
--- src/Text/XSD/Parser.hs
+++ src/Text/XSD/Parser.hs
@@ -2,13 +2,81 @@ module Text.XSD.Parser where
 
 import Control.Exception
 import Control.Monad
+import Control.Monad.Error
+import Control.Monad.Trans.Reader
 import Data.ByteString.Lazy
-import Text.XML
+import Data.Map as M
+import Data.Text
+import Prelude as P
+import Text.XML as XML
+import Text.XML.Cursor as XML
 import Text.XSD.Internal as XSD
 
 
-toXsd :: Document -> Either SomeException XSD.Element
-toXsd = undefined
+newtype XSDException = XSDException String
+  deriving (Show)
+
+instance Exception XSDException
+
+type XSDMonad a = ReaderT (M.Map Name Datatype) (Either SomeException) a
 
 parseXSD :: ParseSettings -> ByteString -> Either SomeException XSD.Element
-parseXSD ps bs = toXsd =<< parseLBS ps bs
+parseXSD ps bs = parseLBS ps bs >>= toXsd
+
+data Expectation
+  = ExpName Text
+  | Expectation :+: Expectation
+
+satisfy :: Expectation -> XML.Node -> Bool
+satisfy exp node@(NodeElement (XML.Element name _ _)) =
+  case node of
+    NodeElement (XML.Element (Name name _ _) _ _) -> case exp of
+      ExpName n     -> name == n
+      exp1 :+: exp2 -> satisfy exp1 node && satisfy exp2 node
+
+satisfySome
+  :: XML.Cursor
+  -> [Expectation]
+  -> Bool
+satisfySome c e = satisfySome' True c e
+
+satisfySome'
+  :: Bool -- ^ check current
+  -> XML.Cursor
+  -> [Expectation]
+  -> Bool
+satisfySome' checkCurrent cur []         = True
+satisfySome' checkCurrent cur (exp:exps) = result
+  where
+    satisfyCurrent = satisfy exp (node cur)
+    validChildren  = P.filter (satisfy (P.head exps) . node) (child cur)
+    result         =
+      if checkCurrent && P.null exps
+      then satisfyCurrent
+      else P.foldr (\c r -> r && satisfySome' False c exps) True validChildren
+
+-- Show is Meh
+throwXsd :: Show s => s -> XSDMonad a
+throwXsd = throwError . SomeException . XSDException . show
+
+toSchema :: XML.Node -> [XSD.Element] -> XSDMonad XSD
+toSchema (XML.NodeElement (XML.Element (Name name _ _) _ _)) elems
+  | name == "schema" = pure $ XSD $ XSD.Element (XSSeq []) "schema" [] elems
+  | otherwise        = throwXsd "Top-level schema definition not found"
+
+fromElem :: XML.Cursor -> XSDMonad XSD.Element
+fromElem cursor = do
+  case node cursor of
+    NodeElement el -> case nameLocalName (elementName el) of
+      "element"    -> do
+
+      "simpleType" -> error "TODO: implement"
+    e              ->
+      throwXsd $ "xsd node not supported: " <> show e
+
+toXsd :: Document -> Either SomeException XSD.Element
+toXsd doc = do
+  let
+    cursor = fromDocument doc
+    elems  = child cursor &| fromElem
+  runReader M.null $ toSchema (node cursor) elems
